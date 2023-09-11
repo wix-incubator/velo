69e8943fd9c05adb399786be3607592cf1d56996
Tom Ben Ezra <tombe@wix.com>
2023-09-11 15:59:58 -0700

    docs: set docusaurus baseUrl config

--- packages/velo-bind-docs/docusaurus.config.js
+++ packages/velo-bind-docs/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/`,
+  baseUrl: `/velo`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -43,7 +43,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl: `https://github.com/${organizationName}/${projectName}/tree/main`,
-          routeBasePath: '/',
+          routeBasePath: "/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
